9726c3f76052bc5588642e53f26a2c4a23622b65
Phil Maurer <phil@aurer.co.uk>
2013-11-25 22:09:42 +0000

    Adding a Notify Command based on the previous versions notify command

---
+++ app/commands/NotifyCommand.php
@@ -0,0 +1,76 @@
+<?php
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class NotifyCommand extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'command:notify';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Notifies users of impending bills.';
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return void
+	 */
+	public function fire()
+	{
+		$users = User::withBillsToday();
+		$this->info( 'Found ' . count($users) . ' users with urgent bills.' );
+
+		foreach ($users as $user) {
+			$this->info('Sending email to ' . $user->email);
+			Mail::send(array('emails.reminder_html', 'emails.reminder_plain'), array('user' => $user), function($message) use ($user) {
+				$message->to($user->email);
+				$message->from('reminders@billbot.aurer.co.uk', 'Billbot');
+			});
+		}
+	}
+
+	/**
+	 * Get the console command arguments.
+	 *
+	 * @return array
+	 */
+	protected function getArguments()
+	{
+		return array(
+			//array('example', InputArgument::REQUIRED, 'An example argument.'),
+		);
+	}
+
+	/**
+	 * Get the console command options.
+	 *
+	 * @return array
+	 */
+	protected function getOptions()
+	{
+		return array(
+			array('test', null, InputOption::VALUE_NONE, null, null),
+		);
+	}
+
+}
